7930ff1f219ff6ac0ddb4d92790dc07a26258474
Alexander Björkén <4242687+bizzno@users.noreply.github.com>
2014-01-09 03:37:00 +0100

    Refactor weatherApi.js

---
+++ src/services/weatherApi.js
@@ -0,0 +1,15 @@
+// Weather API service
+const API_KEY = 'your_api_key_here';
+
+export async function fetchWeather(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching weather:', error);
+    return null;
+  }
+}
+Update 0 on 2014-01-09 03:37:00
